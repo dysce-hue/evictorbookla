9aac52856ff01f63997b27b9f492f26d4b57eae9
dysce-hue <dkguthri@ucsc.edu>
2025-11-30 22:07:07 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1234,7 +1234,7 @@ with tab_hist:
             textposition="outside",
         )
         fig.update_layout(
-            height=370,
+            height=390,
             margin=dict(l=70, r=20, t=70, b=60),
             xaxis_tickformat="$,.0f",
             yaxis_title="Number of Nodes",
