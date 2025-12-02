66724ff64c3db01211296cf5960b862997928867
dysce-hue <dkguthri@ucsc.edu>
2025-12-01 17:11:35 -0800

    Update app.py

--- app.py
+++ app.py
@@ -446,7 +446,7 @@ st.sidebar.markdown(
     .sidebar-creator-badge {
         position: absolute;
         left: 10px;
-        bottom: 1000px;
+        bottom: 100px;
         padding: 4px 10px;
         border-radius: 999px;
         font-size: 11px;
