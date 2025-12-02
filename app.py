5ed464817a250726ccc2e1f4e6706554cf9e1a50
dysce-hue <dkguthri@ucsc.edu>
2025-12-01 17:21:01 -0800

    Update app.py

--- app.py
+++ app.py
@@ -300,7 +300,7 @@ st.markdown(
     .creator-badge {
         position: fixed;
         left: 12px;
-        bottom: -10px;
+        bottom: -20px;
         z-index: 9999;
         padding: 4px 10px;
         border-radius: 999px;
